bd7e9628975b402049530a5a476e18156c320067
DevanshSK <devanshs843@gmail.com>
2024-03-17 19:36:47 +0530

    Added new page and component in monorepo project.

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import Admin from '@repo/ui/admin'
+
+const AdminPage = () => {
+  return (
+    <div>
+        I am an admin page.
+        <Admin />
+    </div>
+  )
+}
+
+export default AdminPage
---
+++ packages/ui/src/admin.tsx
@@ -0,0 +1,9 @@
+
+
+export function Admin() {
+  return (
+    <div>Admin Component</div>
+  )
+}
+
+export default Admin
