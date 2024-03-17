e5208fb9bc5f611760e764288681ee407bac41f8
DevanshSK <devanshs843@gmail.com>
2024-03-17 20:31:14 +0530

    Added a common module for express backend and imported in the express backend.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import {VALUE} from "@repo/common/config"
 
 const app = express()
 
 app.get("/", (req, res) => {
     res.json({
-        message: "hello world"
+        message: VALUE
     });
 })
 
---
+++ packages/common/src/index.ts
@@ -0,0 +1,1 @@
+export const VALUE = "DEVANSH SINGH KUSHWAH"
